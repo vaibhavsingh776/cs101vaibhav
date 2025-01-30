e8fedf08e55d4cd97ea49a2686ce3fdce20632b1
Vaibhav Singh <24b2725@iitb.ac.in>
2025-01-30 22:29:06 +0530

    third one

--- lab3d1/rand.cpp
+++ lab3d1/rand.cpp
@@ -1,13 +1,31 @@
 #include <simplecpp>
-#include<time.h>
+#include<time.h> 
 main_program {
+
 srand(time(NULL));
     int N;
-
     cin >> N;
 
+    // Variables to count occurrences of each face
+    int count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0;
 
+    repeat(N) {
+        int roll = (rand() % 6) + 1; // Generate random number between 1 and 6
 
-    rand() % 6; // Generates a integer number in range 0 to 5
+        // Increment corresponding counter
+        if (roll == 1) count1++;
+        else if (roll == 2) count2++;
+        else if (roll == 3) count3++;
+        else if (roll == 4) count4++;
+        else if (roll == 5) count5++;
+        else count6++;
+    }
 
+    // Output the counts for each face
+    cout << "1: " << count1 << endl;
+    cout << "2: " << count2 << endl;
+    cout << "3: " << count3 << endl;
+    cout << "4: " << count4 << endl;
+    cout << "5: " << count5 << endl;
+    cout << "6: " << count6 << endl;
 }
